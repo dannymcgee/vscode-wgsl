4aded779b3c01c4cc4d3cfaeb86726278a9d34d5
Danny McGee <dannymcgee@gmail.com>
2021-07-17 17:44:23 -0400

    Semantic Tokens

--- packages/server/src/capabilities.rs
+++ packages/server/src/capabilities.rs
@@ -1,9 +1,11 @@
 use lsp_types::{
 	DocumentSymbolOptions, HoverProviderCapability, OneOf, SemanticTokensFullOptions,
-	SemanticTokensLegend, SemanticTokensOptions, SemanticTokensServerCapabilities,
-	ServerCapabilities, TextDocumentSyncCapability, TextDocumentSyncKind, WorkDoneProgressOptions,
+	SemanticTokensOptions, SemanticTokensServerCapabilities, ServerCapabilities,
+	TextDocumentSyncCapability, TextDocumentSyncKind, WorkDoneProgressOptions,
 };
 use serde_json as json;
+
+use crate::semtok;
 
 pub fn define() -> json::Value {
 	json::to_value(&ServerCapabilities {
@@ -12,10 +14,7 @@ pub fn define() -> json::Value {
 			SemanticTokensOptions {
 				full: Some(SemanticTokensFullOptions::Bool(true)),
 				range: None,
-				legend: SemanticTokensLegend {
-					token_types: vec![],     // TODO
-					token_modifiers: vec![], // TODO
-				},
+				legend: semtok::legend(),
 				..Default::default()
 			},
 		)),
