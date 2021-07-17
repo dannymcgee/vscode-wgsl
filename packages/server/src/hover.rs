7b1b071db684c5763cf7abcc2ee8cdae31f516ff
Danny McGee <dannymcgee@gmail.com>
2021-07-17 17:45:56 -0400

    MVP Hover Provider

--- packages/server/src/hover.rs
+++ packages/server/src/hover.rs
@@ -4,27 +4,64 @@ use crossbeam_channel::Sender;
 use lsp_server::{Message, Response};
 use lsp_types::{
 	request::{HoverRequest, Request},
-	Hover, HoverContents, HoverParams, MarkupContent, MarkupKind,
+	Hover, HoverContents, HoverParams, MarkupContent, MarkupKind, Range,
 };
+use parser::{GetRange, IsWithin};
 use serde_json as json;
 
-#[allow(unused_must_use)]
+use crate::documents;
+
 pub fn handle(req: lsp_server::Request, tx: Sender<Message>) {
 	thread::spawn(move || {
-		let (id, _params) = req.extract::<HoverParams>(HoverRequest::METHOD).unwrap();
+		let (id, params) = req.extract::<HoverParams>(HoverRequest::METHOD).unwrap();
 
-		let result = Hover {
-			contents: HoverContents::Markup(MarkupContent {
-				kind: MarkupKind::PlainText,
-				value: "Yep, this is definitely a thing.".into(),
-			}),
-			range: None,
+		let uri = params.text_document_position_params.text_document.uri;
+		let pos = params.text_document_position_params.position;
+		let hover_range = Range {
+			start: pos,
+			end: pos,
 		};
 
-		tx.send(Message::Response(Response {
-			id,
-			result: Some(json::to_value(&result).unwrap()),
-			error: None,
-		}));
+		let result = documents::tokens(&uri)
+			.and_then(|tokens| {
+				tokens
+					.iter()
+					.find(|token| hover_range.is_within(&token.range()))
+					.cloned()
+			})
+			.and_then(|token| {
+				documents::scopes(&uri)
+					.and_then(|scopes| scopes.find_decl(&token))
+					.map(|decl| (token.range(), decl))
+			})
+			.map(|(range, decl)| {
+				let result = Hover {
+					contents: HoverContents::Markup(MarkupContent {
+						kind: MarkupKind::Markdown,
+						value: format!("```\n{}\n```", decl),
+					}),
+					range: Some(range),
+				};
+
+				json::to_value(&result).unwrap()
+			})
+			.or_else(|| {
+				Some(
+					json::to_value(&Hover {
+						contents: HoverContents::Array(vec![]),
+						range: None,
+					})
+					.unwrap(),
+				)
+			});
+
+		#[allow(unused_must_use)]
+		{
+			tx.send(Message::Response(Response {
+				id,
+				result,
+				error: None,
+			}));
+		}
 	});
 }
